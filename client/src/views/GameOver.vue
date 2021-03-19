3c94fc8d9d45de6a7eae164fceb2011583b14d6d
Stewart Tiolamon <iwetiolamon@gmail.com>
2021-03-19 11:15:44 +0700

    navigation guard done

---
+++ client/src/views/GameOver.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <img src="https://media2.giphy.com/media/3ohs4rclkSSrNGSlFK/200.gif" class="rocket">
+    <h1>{{this.$store.state.winner}}</h1>
+    <button type="button" class="btn btn-danger border-dark mt-3" @click.prevent="abort">ABORT MISSION!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GameOver',
+  methods: {
+    abort () {
+      localStorage.removeItem('access_token_1')
+      localStorage.removeItem('access_token_2')
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.rocket {
+  width: 100%
+}
+
+h1 {
+  top: -50px;
+  position: absolute
+}
+</style>
