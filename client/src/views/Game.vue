93a5d314924d115ed2efa8a4402fa9decc14f0c2
bobybewa <bobybewa6@gmail.com>
2021-03-19 13:29:16 +0700

    layout home

--- client/src/views/Game.vue
+++ client/src/views/Game.vue
@@ -7,10 +7,13 @@
           <br>
           <p class="pointP1">{{point1}}</p>
         </div>
-        <img src="https://pngimg.com/uploads/cocacola/cocacola_PNG22.png" class="player1can1" v-if="count === 2">
-        <img src="https://pngimg.com/uploads/cocacola/cocacola_PNG22.png" class="player1can2" v-if="count === 1">
-        <img src="https://pngimg.com/uploads/cocacola/cocacola_PNG22.png" class="player1can3" v-if="count === 0">
-        <button @click.prevent="shake" type="button" class="btn shake1">SHAKE</button>
+        <div class="images1">
+          <img src="https://pngimg.com/uploads/cocacola/cocacola_PNG22.png" class="player1can1" v-if="count === 2">
+          <img src="https://pngimg.com/uploads/cocacola/cocacola_PNG22.png" class="player1can2" v-if="count === 1">
+          <img src="https://pngimg.com/uploads/cocacola/cocacola_PNG22.png" class="player1can3" v-if="count === 0">
+          <button @click.prevent="shake" type="button" class="btn shake1">SHAKE</button>
+        </div>
+        <img class="p1Game" src="https://media3.giphy.com/media/TJmoBmGk4XXkcI6P3t/giphy.gif" alt="test">
       </div>
       <div class="col-6">
         <div class="txt2">
@@ -18,10 +21,13 @@
           <br>
           <p class="pointP2">{{point2}}</p>
         </div>
-        <img src="https://pngimg.com/uploads/pepsi/pepsi_PNG8.png" class="player2can2" v-if="count2 === 1">
-        <img src="https://pngimg.com/uploads/pepsi/pepsi_PNG8.png" class="player2can3" v-if="count2 === 0">
-        <img src="https://pngimg.com/uploads/pepsi/pepsi_PNG8.png" class="player2can1" v-if="count2 === 2">
-        <button @click.prevent="shake2" type="button" class="btn shake2">SHAKE</button>
+        <div class="images2">
+          <img src="https://pngimg.com/uploads/pepsi/pepsi_PNG8.png" class="player2can2" v-if="count2 === 1">
+          <img src="https://pngimg.com/uploads/pepsi/pepsi_PNG8.png" class="player2can3" v-if="count2 === 0">
+          <img src="https://pngimg.com/uploads/pepsi/pepsi_PNG8.png" class="player2can1" v-if="count2 === 2">
+          <button @click.prevent="shake2" type="button" class="btn shake2">SHAKE</button>
+        </div>
+        <img class="p2Game" src="https://i.gifer.com/X5NY.gif" alt="">
       </div>
     </div>
   </div>
@@ -55,107 +61,227 @@ export default {
 </script>
 
 <style>
-.player1can1{
-  display: flex;
-  position: relative;
-  left : 4em;
-  top: 150px;
-  width : 26em;
-  transform: rotate(45deg);
-  -ms-transform: rotate(45deg);
-  -moz-transform: rotate(45deg);
-  -webkit-transform: rotate(45deg);
-  -o-transform: rotate(45deg);
-}
-.player1can2{
-  display: flex;
-  position: relative;
-  left : 4em;
-  top: 10em;
-  width : 26em;
-  transform: rotate(-45deg);
-  -ms-transform: rotate(-45deg);
-  -moz-transform: rotate(-45deg);
-  -webkit-transform: rotate(-45deg);
-  -o-transform: rotate(-45deg);
-}
-.player1can3{
-  display: flex;
-  position: relative;
-  left : 4em;
-  top: 150px;
-  width : 26em;
-}
-.shake1 {
-  position: relative;
-  top : 11.5em;
-  color : white !important;
-  letter-spacing: 2px;
-  background-color : #7A3672!important;
-}
-.player2can1{
-  display: flex;
-  width : 13em;
-  position: relative;
-  left : 9em;
-  top: 8em;
-  transform: rotate(45deg);
-  -ms-transform: rotate(45deg);
-  -moz-transform: rotate(45deg);
-  -webkit-transform: rotate(45deg);
-  -o-transform: rotate(45deg);
-}
-.player2can2{
-  display: flex;
-  width : 13em;
-  position: relative;
-  left : 9em;
-  top: 8em;
-  transform: rotate(-45deg);
-  -ms-transform: rotate(-45deg);
-  -moz-transform: rotate(-45deg);
-  -webkit-transform: rotate(-45deg);
-  -o-transform: rotate(-45deg);
-}
-.player2can3{
-  display: flex;
-  position: relative;
-  left : 9em;
-  top: 8em;
-  width : 13em;
-}
-.shake2 {
-  position: relative;
-  top : 9.7em;
-  left : -0.8em;
-  color : white !important;
-  letter-spacing: 2px;
-  background-color : #7A3672!important;
+
+@media (min-width: 1200px) and (max-width: 1400px) {
+  .p1Game{
+    position: relative;
+    right : 15em;
+    bottom : 9em;
+    width : 374px
+  }
+  .p2Game{
+    position: relative;
+    left : 9em;
+    bottom : 11.5em;
+    width : 300px;
+  }
+  .player1can1{
+      display: flex;
+      position: relative;
+      left : 5em;
+      top: 150px;
+      width : 26em;
+      transform: rotate(45deg);
+      -ms-transform: rotate(45deg);
+      -moz-transform: rotate(45deg);
+      -webkit-transform: rotate(45deg);
+      -o-transform: rotate(45deg);
+  }
+  .player1can2{
+    display: flex;
+    position: relative;
+    left : 5em;
+    top: 10em;
+    width : 26em;
+    transform: rotate(-45deg);
+    -ms-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+    -o-transform: rotate(-45deg);
+  }
+  .player1can3{
+    display: flex;
+    position: relative;
+    left : 5em;
+    top: 150px;
+    width : 26em;
+  }
+  .shake1 {
+    position: relative;
+    top : 11.5em;
+    color : white !important;
+    letter-spacing: 2px;
+    background-color : #7A3672!important;
+  }
+  .player2can1{
+    display: flex;
+    width : 13em;
+    position: relative;
+    left : 9em;
+    top: 8em;
+    transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    -moz-transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+    -o-transform: rotate(45deg);
+  }
+  .player2can2{
+    display: flex;
+    width : 13em;
+    position: relative;
+    left : 9em;
+    top: 8em;
+    transform: rotate(-45deg);
+    -ms-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+    -o-transform: rotate(-45deg);
+  }
+  .player2can3{
+    display: flex;
+    position: relative;
+    left : 9em;
+    top: 8em;
+    width : 13em;
+  }
+  .shake2 {
+    position: relative;
+    top : 9.7em;
+    left : -0.8em;
+    color : white !important;
+    letter-spacing: 2px;
+    background-color : #7A3672!important;
+  }
+
+  .txt {
+    position: relative;
+    top : 6.5em;
+    color : #E4ACE5;
+  }
+
+  .pointP1 {
+    font-size : 30px;
+    margin-top : -1em;
+    margin-bottom: -1em;
+  }
+
+  .txt2 { 
+    position: relative;
+    top : 6em;
+    left: -1em;
+    color : #E4ACE5;
+  }
+
+  .pointP2 {
+    font-size : 30px;
+    margin-top : -1em;
+    margin-bottom: -1em;
+  }
 }
 
-.txt {
-  position: relative;
-  top : 6.5em;
-  color : #E4ACE5;
-}
+/* @media (min-width: 950px)  { 
+  .player1can1{
+      display: flex;
+      position: relative;
+      left : 1.8em;
+      top: 150px;
+      width : 26em;
+      transform: rotate(45deg);
+      -ms-transform: rotate(45deg);
+      -moz-transform: rotate(45deg);
+      -webkit-transform: rotate(45deg);
+      -o-transform: rotate(45deg);
+  }
+  .player1can2{
+    display: flex;
+    position: relative;
+    left : 1.8em;
+    top: 10em;
+    width : 26em;
+    transform: rotate(-45deg);
+    -ms-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+    -o-transform: rotate(-45deg);
+  }
+  .player1can3{
+    display: flex;
+    position: relative;
+    left : 1.8em;
+    top: 150px;
+    width : 26em;
+  }
+  .shake1 {
+    position: relative;
+    top : 11.5em;
+    color : white !important;
+    letter-spacing: 2px;
+    background-color : #7A3672!important;
+  }
+  .player2can1{
+    display: flex;
+    width : 13em;
+    position: relative;
+    left :7em;
+    top: 8em;
+    transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    -moz-transform: rotate(45deg);
+    -webkit-transform: rotate(45deg);
+    -o-transform: rotate(45deg);
+  }
+  .player2can2{
+    display: flex;
+    width : 13em;
+    position: relative;
+    left :7em;
+    top: 8em;
+    transform: rotate(-45deg);
+    -ms-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -webkit-transform: rotate(-45deg);
+    -o-transform: rotate(-45deg);
+  }
+  .player2can3{
+    display: flex;
+    position: relative;
+    left :7em;
+    top: 8em;
+    width : 13em;
+  }
+  .shake2 {
+    position: relative;
+    top : 9.7em;
+    left : -0.8em;
+    color : white !important;
+    letter-spacing: 2px;
+    background-color : #7A3672!important;
+  }
 
-.pointP1 {
-  font-size : 30px;
-  margin-top : -1em;
-  margin-bottom: -1em;
-}
+  .txt {
+    position: relative;
+    top : 6.5em;
+    color : #E4ACE5;
+  }
 
-.txt2 { 
-  position: relative;
-  top : 6em;
-  left: -1em;
-  color : #E4ACE5;
-}
+  .pointP1 {
+    font-size : 30px;
+    margin-top : -1em;
+    margin-bottom: -1em;
+  }
 
-.pointP2 {
-  font-size : 30px;
-  margin-top : -1em;
-  margin-bottom: -1em;
-}
+  .txt2 { 
+    position: relative;
+    top : 6em;
+    left: -1em;
+    color : #E4ACE5;
+  }
+
+  .pointP2 {
+    font-size : 30px;
+    margin-top : -1em;
+    margin-bottom: -1em;
+  }
+} */
 
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,28 +4,30 @@
       <div class="col-6 vh-100 t1">
         <div class="player1">
           <img class="p1" src="https://media3.giphy.com/media/TJmoBmGk4XXkcI6P3t/giphy.gif" alt="test">
-        <button type="button" class="btn btn1">Player 1</button>
+          <button type="button" class="btn btn1">Player 1</button>
         </div>
         <div class="player2">
           <img class="p2" src="https://i.gifer.com/X5NY.gif" alt="">
-        <button type="button" class="btn btn2">Player 2</button>
+          <button type="button" class="btn btn2">Player 2</button>
         </div>
       </div>
       <div class="col-6 vh-100 right">
-      <div class="text-rules">
-        <h3>Aturan Bermain :</h3>
-        <ol>
-          <li class="rules">untuk bermain, kedua player harus menekan tombol terlebih dahulu</li>
-          <li class="rules">pemain akan mendapatkan dengan menekan tombol</li>
-          <li class="rules">kumpulkan point sebanyak 100, yang mendapatkan point 100 terlebih dahulu maka dia lah pemenang nya</li>
-        </ol>
-      </div>
+        <div class="text-rules">
+          <h3>Aturan Bermain :</h3>
+          <ol>
+            <li class="rules">untuk bermain, kedua player harus menekan tombol terlebih dahulu</li>
+            <li class="rules">pemain akan mendapatkan dengan menekan tombol</li>
+            <li class="rules">kumpulkan point sebanyak 100, yang mendapatkan point 100 terlebih dahulu maka dia lah pemenang nya</li>
+          </ol>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+
+@media (min-width: 1100px) and (max-width: 1400px) { 
   *{
       font-family: 'Poppins', sans-serif;
       margin: 0px;
@@ -71,12 +73,284 @@
     color : white;
     letter-spacing: 2px;
     position: relative;
-    left : 7px;
+    left : 1px;
     background-color : #7A3672!important;
   }
   .right{
     background-color: #1A1A48;
   }
+}
+
+@media (min-width: 992px) { 
+    .p2 {
+      position: relative;
+      width : 26em;
+      left: -37px;
+    }
+    .player2 {
+      position: relative;
+      top:  45px;
+    }
+    .p1 {
+      width : 26em;
+      position: relative;
+      left: -20px;
+    }
+
+    .rules{
+        text-align : left;
+        margin-left : 20px;
+        margin-top: 15px;
+    }
+    .text-rules{
+      position: relative;
+      top : 70px;
+      color : white;
+    }
+    h3{
+      font-family: Arial, Helvetica, sans-serif;
+      letter-spacing: 2px;
+    }
+    li{
+      font-family: Arial, Helvetica, sans-serif;
+    }
+    .btn1{
+      position: relative;
+      top: 2em;
+      left : 5px;
+      color : white;
+      letter-spacing: 2px;
+      background-color : #7A3672!important;
+    }
+    .btn2{
+      color : white;
+      letter-spacing: 2px;
+      position: relative;
+      left : 19px;
+      top : -8px;
+      background-color : #7A3672!important;
+    }
+    .right{
+      background-color: #1A1A48;
+    }
+}
+
+@media (min-width : 778px) {
+  .p2 {
+      position: relative;
+      width : 26em;
+      left: -37px;
+    }
+    .player2 {
+      position: relative;
+      top:  45px;
+    }
+    .p1 {
+      width : 26em;
+      position: relative;
+      left: -20px;
+    }
+
+    .rules{
+        text-align : left;
+        margin-left : 20px;
+        margin-top: 15px;
+    }
+    .text-rules{
+      position: relative;
+      top : 70px;
+      color : white;
+    }
+    h3{
+      font-family: Arial, Helvetica, sans-serif;
+      letter-spacing: 2px;
+    }
+    li{
+      font-family: Arial, Helvetica, sans-serif;
+    }
+    .btn1{
+      position: relative;
+      top: 2em;
+      left : 5px;
+      color : white;
+      letter-spacing: 2px;
+      background-color : #7A3672!important;
+    }
+    .btn2{
+      color : white;
+      letter-spacing: 2px;
+      position: relative;
+      left : 18px;
+      top : -8px;
+      background-color : #7A3672!important;
+    }
+    .right{
+      background-color: #1A1A48;
+    }
+}
+
+@media (min-width : 920px){
+  .p2 {
+      position: relative;
+      width : 26em;
+      left: -37px;
+    }
+    .player2 {
+      position: relative;
+      top:  45px;
+    }
+    .p1 {
+      width : 26em;
+      position: relative;
+      left: -20px;
+    }
+
+    .rules{
+        text-align : left;
+        margin-left : 20px;
+        margin-top: 15px;
+    }
+    .text-rules{
+      position: relative;
+      top : 70px;
+      color : white;
+    }
+    h3{
+      font-family: Arial, Helvetica, sans-serif;
+      letter-spacing: 2px;
+    }
+    li{
+      font-family: Arial, Helvetica, sans-serif;
+    }
+    .btn1{
+      position: relative;
+      top: 2em;
+      left : 5px;
+      color : white;
+      letter-spacing: 2px;
+      background-color : #7A3672!important;
+    }
+    .btn2{
+      color : white;
+      letter-spacing: 2px;
+      position: relative;
+      left : 18px;
+      top : -8px;
+      background-color : #7A3672!important;
+    }
+    .right{
+      background-color: #1A1A48;
+    }
+}
+
+@media (min-width: 650px) { 
+    .p2 {
+        position: relative;
+        width : 26em;
+        left: -37px;
+    }
+    .player2 {
+      position: relative;
+      top:  45px;
+    }
+    .p1 {
+      width : 26em;
+      position: relative;
+      left: -20px;
+    }
+
+    .rules{
+        text-align : left;
+        margin-left : 20px;
+        margin-top: 15px;
+    }
+    .text-rules{
+      position: relative;
+      top : 70px;
+      color : white;
+    }
+    h3{
+      font-family: Arial, Helvetica, sans-serif;
+      letter-spacing: 2px;
+    }
+    li{
+      font-family: Arial, Helvetica, sans-serif;
+    }
+    .btn1{
+      position: relative;
+      top: 2em;
+      left : 5px;
+      color : white;
+      letter-spacing: 2px;
+      background-color : #7A3672!important;
+    }
+    .btn2{
+      color : white;
+      letter-spacing: 2px;
+      position: relative;
+      left : 18px;
+      top : -8px;
+      background-color : #7A3672!important;
+    }
+    .right{
+      background-color: #1A1A48;
+    } 
+}
+
+@media (min-width: 576px)  { 
+    .p2 {
+      position: relative;
+      width : 26em;
+      left: -113px;
+    }
+    .player2 {
+      position: relative;
+      top:  45px;
+    }
+    .p1 {
+      width : 26em;
+      position: relative;
+      left: -85px;
+    }
+
+    .rules{
+        text-align : left;
+        margin-left : 20px;
+        margin-top: 15px;
+    }
+    .text-rules{
+      position: relative;
+      top : 70px;
+      color : white;
+    }
+    h3{
+      font-family: Arial, Helvetica, sans-serif;
+      letter-spacing: 2px;
+    }
+    li{
+      font-family: Arial, Helvetica, sans-serif;
+    }
+    .btn1{
+      position: relative;
+      top: 2em;
+      left : -1em;
+      color : white;
+      letter-spacing: 2px;
+      background-color : #7A3672!important;
+    }
+    .btn2{
+      color : white;
+      letter-spacing: 2px;
+      position: relative;
+      left : -0.7em;
+      top : -8px;
+      background-color : #7A3672!important;
+    }
+    .right{
+      background-color: #1A1A48;
+    }
+}
+
 </style>
 
 
